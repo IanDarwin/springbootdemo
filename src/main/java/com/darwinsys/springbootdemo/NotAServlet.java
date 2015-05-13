d380ab610a42d8614d21c86e265394aa35f1c4ce
Ian Darwin <ian@darwinsys.com>
2015-05-12 21:09:53 -0400

    Clean up; use ModelMap not Model; only GET method.

--- src/main/java/com/darwinsys/springbootdemo/NotAServlet.java
+++ src/main/java/com/darwinsys/springbootdemo/NotAServlet.java
@@ -1,16 +1,18 @@
 package com.darwinsys.springbootdemo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller()
 public class NotAServlet {
 
-	@RequestMapping("/notaservlet")
-	protected String doWork(@RequestParam(value="name") String name, Model model) {
-		model.addAttribute("greets", String.format("<html><h1>Hello %s!!</h1>", name));
+	@RequestMapping(value="/notaservlet", method=RequestMethod.GET)
+	public String doWork(@RequestParam("name") String name, ModelMap model) {
+		System.out.println("NotAServlet.doWork()");
+		model.addAttribute("greetings", String.format("<html><h1>Hello %s!!</h1>", name));
 		return "hello";
 	}
 }
