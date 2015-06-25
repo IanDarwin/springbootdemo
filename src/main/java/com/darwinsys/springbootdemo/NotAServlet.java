43c647e083ca645d0f2edaaf4289a1222d4906eb
Ian Darwin <ian@darwinsys.com>
2015-06-25 16:16:55 +0100

    Add doc comment for cryptic name.

--- src/main/java/com/darwinsys/springbootdemo/NotAServlet.java
+++ src/main/java/com/darwinsys/springbootdemo/NotAServlet.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ * It's not a Servlet, it's a Controller
+ */
 @Controller()
 public class NotAServlet {
 
